542f61318b222a2b583e3d161feb0f35ac1eb7d7
Akash Jaiswal <akashjai0009@gmail.com>
2023-08-13 22:03:31 +0530

    first commit

---
+++ index.js/node.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+
+const os = require('os');
+
+// console.log(__dirna
+console.log(os.type())
+console.log(os.hostname())
+console.log(os.homedir())
+console.log(os.version())
+
+
+const path = require('path')
+console.log(path.dirname(__filename))
